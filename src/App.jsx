ccdccc0e03bdd0f0ef29735bd906b85d8e91e539
ElmurodAzodov <elmurodazodov002@gmail.com>
2025-08-03 10:52:00 +0500

    react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import react from 'react';
-import {Routes, Route} from 'react-router-dom';
-import Navbar from './components/navbar';
-import Home from './pages/Home';
-import Products from './pages/Products';
-import About from './pages/About';
-import Contact from './pages/Contact';
-
+import react from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
     <div>
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="" width={130} height={30} />
+      <img src={logo} alt="" width="130px" />
       <ul>
-        <li>Home</li>
-        <li>Products</li>
-        <li>About</li>
-        <li>Contact</li>
+        <Link to="/"><li>Home</li></Link>
+        <Link to="/products"><li>Products</li></Link>
+        <Link to="/about"><li>About</li></Link>
+        <Link to="/contact"><li>Contact</li></Link>
       </ul>
       <button>Get Started</button>
     </div>
